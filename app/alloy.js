84875c4b6c61af547270a68b4d79e1f17898bf56
Soft M <Admin@MacBook-Pro-de-Soft-3.local>
2016-11-12 12:01:48 -0600

    Se crean short functions para imprimir en consola.
    Signed-off-by: Soft M <Admin@MacBook-Pro-de-Soft-3.local>

--- app/alloy.js
+++ app/alloy.js
@@ -11,8 +11,12 @@
 // Alloy.Globals.someGlobalFunction = function(){};
 
 var print =  console.log;
+var jstr = JSON.stringify;
+var print_json = function (json) {
+	print(jstr(json));
+};
 
-var IP_SERVER = "192.168.1.68";
+var IP_SERVER = "192.168.8.149";
 var PORT = ":3000";
 var END_POINT = IP_SERVER + PORT;
 var WS_GET_FEED = END_POINT + "/feed";
