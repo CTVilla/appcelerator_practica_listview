52d51fe6f87ada2674f9cd8286403330bbb4c93c
Ceferino Torres Villa <ceferino_torres@migesa.com.mx>
2016-11-18 18:52:42 -0600

    Se agregan comentarios a las funciones globales.
    Signed-off-by: Ceferino Torres Villa <ceferino_torres@migesa.com.mx>

--- app/alloy.js
+++ app/alloy.js
@@ -9,13 +9,48 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
-
-var print = function(o) {
-	console.log(o);
-};
-var tojson = JSON.stringify;
-
-var IP_SERVER = "192.168.0.20";
+/**
+ * Variables for the connection with the web services.
+ */
+var IP_SERVER = "192.168.0.19";
 var PORT = ":3000";
 var END_POINT = IP_SERVER + PORT;
 var WS_GET_FEED = END_POINT + "/feed";
+/**
+ * Request Methods.
+ * This variables are used to define the type in request method
+ */
+var GET = 'GET';
+var POST = 'POST';
+var PUT = 'PUT';
+var DELETE = 'DELETE';
+
+/**
+ * Function to print in console
+ * @param {Object} o Object to print
+ */
+var print = function(o) {
+	console.log(o);
+};
+/**
+ * Anonimous function to Stringtify
+ */
+var tojson = JSON.stringify;
+
+/**
+ * Anonimous function to Fetch the data
+ * in a listview
+ * @param {Object} listview Listview element
+ * @param {Object} method Request method(GET, POST, PUT, DELETE)
+ * @param {Object} isLocalOnly Boolean to define if the data could be saved locally.
+ * @param {Object} successCallback Function to execute in success case.
+ * @param {Object} errorCallback Function to execute in error case.
+ */
+var fetchListview = function (listview, method, isLocalOnly, successCallback, errorCallback){
+	listview.fetch({
+		requestMethod: method,
+		localOnly:isLocalOnly,		
+		success:successCallback,
+		error:errorCallback
+	});
+};
